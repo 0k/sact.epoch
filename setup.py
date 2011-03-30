0ef7e0098437b36309b89c7f9cfc614ef5ab3860
Valentin Lab <valentin.lab@securactive.net>
2011-03-30 10:48:20 +0200

    chg: dev: prepared ``setup.py`` for release on github and pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
+
 def get_version(version):
     import datetime
     if "dev" in version:
@@ -14,16 +15,20 @@ setup(
     name='sact.epoch',
     version=get_version(version),
     description="",
-    long_description=open("docs/source/overview.txt").read(),
+    long_description=open("README.rst").read() + open("CHANGELOG.rst").read(),
     # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development",
     ],
-    keywords='sact nss4',
+    keywords='sact time timedelta',
     author='SecurActive SA',
-    author_email='dev@securactive.net',
-    url='www.securactive.net',
+    author_email='opensource@securactive.net',
+    url='http://github.com/securactive/sact.epoch',
     license='SecurActive license',
     package_dir={'': 'src'},
     packages=find_packages('src'),
